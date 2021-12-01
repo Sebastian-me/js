1f531f3b51999358e03de456f5484b97a19f6369
sschmitz <s.schmitz@neusta.de>
2021-12-01 11:34:41 +0100

    js angular list

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import {Hero} from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Dr. Who' },
+  { id: 2, name: 'Saitama' },
+  { id: 3, name: 'Jack Sparrow' },
+  { id: 4, name: 'Genos' },
+  { id: 5, name: 'Silverfang' },
+  { id: 6, name: 'Sheev Palpatine' },
+  { id: 7, name: 'Dr. Strange' },
+  { id: 8, name: 'Tornado' }
+]
