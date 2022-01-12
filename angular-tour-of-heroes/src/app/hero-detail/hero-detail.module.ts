531ec796a506bd6eabb2bbb9d8bb63519a7ea4cb
sschmitz <s.schmitz@neusta.de>
2022-01-12 10:12:43 +0100

    added messages & details for heroes

---
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HeroDetailComponent} from './hero-detail.component';
+
+@NgModule({
+    imports: [
+        CommonModule
+    ],
+    declarations: [
+    ],
+    exports: [
+    ]
+})
+export class HeroDetailModule {}
